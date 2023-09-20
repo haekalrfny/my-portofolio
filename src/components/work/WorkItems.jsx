e6ec4d97092542b70e9fd853f3b8c3f650cf0e21
haekalrfny <haekalrfny@gmail.com>
2023-09-20 19:02:08 +0700

    portofolio

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -7,7 +7,7 @@ const WorkItems = ({ item }) => {
       <img src={item.image} alt="" className="work__img" />
       <div className="work__padding">
         <h3 className="work__title">{item.title}</h3>
-        <a href="#" className="work__button">
+        <a href={item.link} target="_blank" className="work__button">
           Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
       </div>
