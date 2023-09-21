4a4abcbce9cb1030c09e95e83f2234ca92ec79e5
haekalrfny <haekalrfny@gmail.com>
2023-09-21 10:18:06 +0700

    target change

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "../../App.css"
 
 const WorkItems = ({ item }) => {
+  const target = item.link === "#" ? null : "_blank";
   return (
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <div className="work__padding">
         <h3 className="work__title">{item.title}</h3>
-        <a href={item.link} target="_blank" className="work__button">
+        <a href={item.link} target={target} className="work__button">
           Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
       </div>
