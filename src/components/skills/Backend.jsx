84060e0d19052b9f6f19730d1cf94efd5220c4bb
Your Name <you@example.com>
2024-02-22 13:36:45 +0700

    skills

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -4,7 +4,7 @@ import "../../App.css";
 const Backend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Web (Backend)</h3>
+      <h3 className="skills__title">Backend</h3>
       <div className="skills__box">
         <div className="skills__group">
           <div className="skills__data">
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -4,7 +4,7 @@ import "../../App.css";
 const Frontend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Web (Frontend)</h3>
+      <h3 className="skills__title">Frontend</h3>
       <div className="skills__box">
         <div className="skills__group">
           <div className="skills__data">
